0133b2942feb01170c442a06f591fecfff2ea3b9
daviddong091216 <daviddong201712@gmail.com>
2023-12-11 10:01:22 -0600

    20231211-1

---
+++ src/com/dong/ArraysDemo.java
@@ -0,0 +1,40 @@
+package com.dong;
+
+public class ArraysDemo {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int[] arr;
+		arr = new int[10];
+		// System.out.println(arr);
+		// System.out.println(arr[0]);
+
+		int[] arr2;
+		// arr2= {1,2,3};
+
+		int[] arr3 = { 1, 2, 3 };
+		// System.out.println(arr3);
+		// System.out.println(arr3[1]);
+
+		byte[] b = new byte[10];
+		System.out.println(b[0]);
+		short[] s = new short[10];
+		System.out.println(s[0]);
+		int[] i = new int[10];
+		System.out.println(i[0]);
+		long[] l = new long[10];
+		System.out.println(l[0]);
+		float[] f = new float[10];
+		System.out.println(f[0]);
+		double[] d = new double[10];
+		System.out.println(d[0]);
+		char[] c = new char[10];
+		System.out.println(c[0]);
+		boolean[] b2 = new boolean[10];
+		System.out.println(b2[0]);
+
+		int[][] m = new int[3][5];
+		System.out.println(m);
+	}
+
+}
--- src/com/dong/PrimitiveTypes.java
+++ src/com/dong/PrimitiveTypes.java
@@ -4,7 +4,28 @@ public class PrimitiveTypes {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
+		byte b = 1;
+		short s;
+		s = 2;
+		long l2 = 2_000_000_000;
+		long l3 = 2_000_000_000_000_000L;
 
+		float f1 = (float) 1.2;
+		float f2 = 1.2F;
+		float f3 = 1.2f;
+
+		char c1 = 'a';
+		char c2 = 100;
+		System.out.println(c2);
+		char c3 = 1000;
+		System.out.println(c3);
+		//
+		// overfloat
+		byte b2 = (byte) 130;
+		System.out.println(b2);
+
+		int hex = 0x98;
+		System.out.println(hex);
 	}
 
 }
